01a39685ffa7b112bbb00c9539bd922e18fcec99
Ron Bronson <contact@ronbronson.com>
2022-10-06 18:13:54 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,15 +8,6 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon.png'
-            />
-
-            <link rel='manifest' href='/manifest.json' />
           </Head>
 
           <body>
